75dacbd3094d9704a64700e41b73f645d512a679
liliwang <monsi0510@hotmail.com>
2020-10-08 13:36:39 -0500

    Update extra array exercises

--- src/extraarrayexercises/GroceryList.java
+++ src/extraarrayexercises/GroceryList.java
@@ -1,2 +1,33 @@
-package extraarrayexercises;public class GeroceryList {
+package extraarrayexercises;
+
+import util.Input;
+
+public class GroceryList {
+    /*1. Create a command line grocery list maker
+        - before prompting the user to enter each item, ask how many items will be
+          entered
+        - for each item, the user should be prompted to enter in the name, quantity,
+          and category of each grocery item
+        - Create a `GroceryItem` class from which to instantiate `GroceryItem`
+          objects to store in an array
+        - once the user has entered all items, print out all items grouped by
+          category and alphabetized in each group*/
+    public static void main(String[] args) {
+        Input input = new Input();
+        int itemsNumber = input.getInt("How many items will be entered into the grocery list?");
+
+        GroceryItem[] groceryItems = new GroceryItem[itemsNumber];
+
+        for (int i = 0; i < itemsNumber; i++) {
+            groceryItems[i] = new GroceryItem();
+            groceryItems[i].setName(input.getString("Enter name of the grocery item: "));
+            groceryItems[i].setQuantity(input.getInt("Enter quantity of the grocery item: "));
+            groceryItems[i].setCategory(input.getString("Enter category of the grocery item: "));
+        }
+    }
+
+    public void displayGrocery(GroceryItem[] groceryArr) {
+
+    }
+
 }
--- src/extraarrayexercises/TicToc.java
+++ src/extraarrayexercises/TicToc.java
@@ -1,2 +1,20 @@
-package extraarrayexercises;public class TicToc {
+package extraarrayexercises;
+
+public class TicToc {
+    /*1. Build a command line Tic Tac Toe game.
+    - the game board should be a multi-dimensional array of Strings
+    - be sure the board displays the column letter and row number:
+                A   B   C
+            1  -O- --- ---
+            2  --- -X- ---
+            3  --- --- ---
+    - the "O" in the example above is at 1a and the "X" is at 2B
+    - the winning three in a row should match should be drawn with stars instead
+      of dashes:
+                A   B   C
+            1  *O* *O* *O*
+            2  --- -X- ---
+            3  -X- --- ---
+    - player one and player two should alternate turns
+    - be sure to display who's turn it is and who won*/
 }
